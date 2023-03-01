90c978d2e573ef17b9ae1747dbaa1700ad1dd8e9
karenpyg <karen2003panahi@gmail.com>
2023-03-01 17:23:20 +0330

    added dp

---
+++ dynamic_programming/vacation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+const int MAX_INT = 1e5; 
+int dp[MAX_INT][3]; 
+
+int main()
+{
+    memset(dp, -1, sizeof(dp));
+    
+    int N ; cin >> N; 
+    for (int i = 0; i < N; i++){
+        int a, b, c ; cin >> a >> b >> c ; 
+
+
+    }
+}
